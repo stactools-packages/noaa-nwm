49ca48f5bc05888379e6f73ec2f4afefd139aafa
Tom Augspurger <taugspurger@microsoft.com>
2023-10-14 16:37:33 -0500

    [WIP]: Initial prototype

--- src/stactools/noaa_nwm/stac.py
+++ src/stactools/noaa_nwm/stac.py
@@ -1,67 +1,159 @@
-from datetime import datetime, timezone
+import pystac
+import re
+import typing
+import dataclasses
+import datetime
+import enum
+import fsspec
+import xarray as xr
+import xstac
 
-import stactools.core.create
-from pystac import (
-    Collection,
-    Extent,
-    Item,
-    SpatialExtent,
-    TemporalExtent,
-)
+
+class Product(str, enum.Enum):
+    CHANNEL_RT = "channel_rt"
+    LAND = "land"
+    RESERVOIR = "reservoir"
+    TERRAIN_RT = "terrain_rt"
 
 
-def create_collection() -> Collection:
-    """Creates a STAC Collection.
+class Region(str, enum.Enum):
+    CONUS = "conus"
 
-    This function should create a collection for this dataset. See `the STAC
-    specification
-    <https://github.com/radiantearth/stac-spec/blob/master/collection-spec/collection-spec.md>`_
-    for information about collection fields, and
-    `Collection<https://pystac.readthedocs.io/en/latest/api.html#collection>`_
-    for information about the PySTAC class.
 
-    Returns:
-        Collection: STAC Collection object
+class Category(str, enum.Enum):
+    SHORT_RANGE = "short_range"
+
+
+@dataclasses.dataclass
+class NWMInfo:
     """
-    extent = Extent(
-        SpatialExtent([[-180.0, 90.0, 180.0, -90.0]]),
-        TemporalExtent([[datetime.now(tz=timezone.utc), None]]),
+    Examples
+    --------
+    >>> info = NWMInfo.from_filename(
+    ...     "nwm.20231010/short_range/nwm.t00z.short_range.channel_rt.f001.conus.nc"
+    ... )
+    >>> info
+    """
+
+    date: datetime.datetime
+    category: Category  # literal / enum
+    cycle_runtime: int  # literal / enum
+    product: Product
+    forecast_hour: int
+    region: Region
+
+    pattern = re.compile(
+        "nwm\.(?P<date>\d{8})/"
+        "(?P<category>short_range)"
+        "/nwm\.t(?P<cycle_runtime>"
+        "\d{2})z\.short_range\."
+        "(?P<product>(channel_rt|land|reservoir|terrain_rt))\."
+        "f(?P<forecast_hour>\d{3})\."
+        "(?P<region>conus)\.nc"
     )
 
-    collection = Collection(
-        id="example-collection",
-        title="Example collection",
-        description="An example collection",
-        extent=extent,
-        extra_fields={"custom_attribute": "foo"},
+    @classmethod
+    def from_filename(cls, s: str):
+        m = cls.pattern.match(s)
+        assert m
+        d = m.groupdict()
+
+        date = datetime.datetime.strptime(d["date"], "%Y%m%d")
+        category = Category(d["category"])
+        cycle_runtime = int(d["cycle_runtime"])
+        forecast_hour = int(d["forecast_hour"])
+        region = Region(d["region"])
+        product = Product(d["product"])
+
+        return cls(
+            date=date,
+            category=category,
+            cycle_runtime=cycle_runtime,
+            forecast_hour=forecast_hour,
+            region=region,
+            product=product,
+        )
+
+    @property
+    def filename(self):
+        # reconstruct it
+        ...
+
+    @property
+    def id(self):
+        return "-".join(
+            [
+                self.date.strftime("%Y%m%d"),
+                self.category,
+                self.region,
+                self.product,
+                str(self.cycle_runtime),
+                str(self.forecast_hour),
+            ]
+        )
+
+    @property
+    def datetime(self):
+        # TODO: confirm this is correct
+        return self.date + datetime.timedelta(hours=self.forecast_hour)
+
+    @property
+    def extra_properties(self):
+        # TODO: use the forecast extension
+        return {
+            "nwm:category": self.category.value,
+            "nwm:region": self.region.value,
+            "nwm:product": self.product.value,
+            "nwm:forecast_hour": self.forecast_hour,
+        }
+
+
+def create_item(
+    href: str,
+    read_href_modifier=None,
+    kerchunk_indices: dict[str, typing.Any] | None = None,
+):
+    path = "/".join(href.rsplit("/", 3)[-3:])
+    info = NWMInfo.from_filename(path)
+
+    ds = xr.open_dataset(fsspec.open(href).open())
+    ds = xstac.fix_attrs(ds)
+
+    # TODO: geometry from the region (conus, ...)
+    template = pystac.Item(
+        info.id,
+        geometry=None,
+        bbox=None,
+        datetime=info.datetime,
+        properties=dict(info.extra_properties),
     )
-    return collection
 
+    additional_dimensions = {
+        "feature_id": {
+            "type": "ID",
+            "description": ds.feature_id.attrs["comment"],
+            "extent": [None, None],
+        },
+        "reference_time": {
+            "type": "reference-time",
+            "description": ds.reference_time.attrs["long_name"],
+            "extent": [None, None],
+        },
+    }
 
-def create_item(asset_href: str) -> Item:
-    """Creates a STAC item from a raster asset.
+    item = xstac.xarray_to_stac(
+        ds,
+        template,
+        x_dimension=False,
+        y_dimension=False,
+        kerchunk_indices=kerchunk_indices,
+        **additional_dimensions,
+    )
 
-    This example function uses :py:func:`stactools.core.utils.create_item` to
-    generate an example item.  Datasets should customize the item with
-    dataset-specific information, e.g.  extracted from metadata files.
+    item.assets["data"] = pystac.Asset(href=href, media_type="application/x-netcdf")
 
-    See `the STAC specification
-    <https://github.com/radiantearth/stac-spec/blob/master/item-spec/item-spec.md>`_
-    for information about an item's fields, and
-    `Item<https://pystac.readthedocs.io/en/latest/api/pystac.html#pystac.Item>`_ for
-    information on the PySTAC class.
-
-    This function should be updated to take all hrefs needed to build the item.
-    It is an anti-pattern to assume that related files (e.g. metadata) are in
-    the same directory as the primary file.
-
-    Args:
-        asset_href (str): The HREF pointing to an asset associated with the item
-
-    Returns:
-        Item: STAC Item object
-    """
-    item = stactools.core.create.item(asset_href)
-    item.id = "example-item"
-    item.properties["custom_attribute"] = "foo"
     return item
+
+
+def create_collection():
+    ...
