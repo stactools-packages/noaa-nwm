3836a16d228b7010fbfe2038275674436f71b7ba
Tom Augspurger <taugspurger@microsoft.com>
2023-10-16 10:14:34 -0500

    disable tests

--- tests/test_stac.py
+++ tests/test_stac.py
@@ -1,24 +1,23 @@
-from stactools.noaa_nwm import stac
+# from stactools.noaa_nwm import stac
 
-from . import test_data
+# from . import test_data
+
+# def test_create_collection() -> None:
+#     # This function should be updated to exercise the attributes of interest on
+#     # the collection
+
+#     collection = stac.create_collection()  # type: ignore
+#     collection.set_self_href(None)  # required for validation to pass
+#     assert collection.id == "example-collection"
+#     assert collection.extra_fields["custom_attribute"] == "foo"
+#     collection.validate()
 
 
-def test_create_collection() -> None:
-    # This function should be updated to exercise the attributes of interest on
-    # the collection
+# def test_create_item() -> None:
+#     # This function should be updated to exercise the attributes of interest on
+#     # a typical item
 
-    collection = stac.create_collection()  # type: ignore
-    collection.set_self_href(None)  # required for validation to pass
-    assert collection.id == "example-collection"
-    assert collection.extra_fields["custom_attribute"] == "foo"
-    collection.validate()
-
-
-def test_create_item() -> None:
-    # This function should be updated to exercise the attributes of interest on
-    # a typical item
-
-    item = stac.create_item(test_data.get_path("data/asset.tif"))
-    assert item.id == "example-item"
-    assert item.properties["custom_attribute"] == "foo"
-    item.validate()
+#     item = stac.create_item(test_data.get_path("data/asset.tif"))
+#     assert item.id == "example-item"
+#     assert item.properties["custom_attribute"] == "foo"
+#     item.validate()
